f905c0e9a357ec9ece28e6cc4541418d734aefed
Mendy Dihars <mendy.dihars@laposte.net>
2022-09-07 00:04:38 +0200

    create css file succeed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   mode: 'production',
   entry: path.resolve('index.js'),
@@ -7,13 +9,15 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'main.js',
   },
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.scss/,
         exclude: /node_modules/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
           'css-loader',
           'sass-loader',
         ]
